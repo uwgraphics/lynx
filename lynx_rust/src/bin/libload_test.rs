fa411c58962622feed964cf7daa03e70e15bbaea
djrakita <djrakita@gmail.com>
2021-04-10 17:12:07 -0500

    change gitignore

--- lynx_rust/src/bin/libload_test.rs
+++
@@ -1,8 +0,0 @@
-use libloading;
-
-fn main() {
-    unsafe {
-        let lib = libloading::Library::new("/home/rakita/lynx/lynx_rust/src/bin/libnn_lib.so");
-        let lib_unwrap = lib.as_ref().ok().unwrap();
-    }
-}
